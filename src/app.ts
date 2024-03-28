a6c98871f6f03b61240bcc2d36abf0bf26d87472
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2024-03-28 11:54:30 -0500

    feat: Server files using http2, secure server and certificates

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,38 @@
-import { readFileSync } from 'fs';
-import http from 'http';
+import fs from 'fs';
+import http2 from 'http2';
 
-const server = http.createServer((req, res) => {
-	// res.writeHead(200, { 'Content-Type': 'text/html' });
-	// res.write('<h1>Hello</h1>');
-	// res.end();
+const server = http2.createSecureServer(
+	{
+		key: fs.readFileSync('./keys/server.key'),
+		cert: fs.readFileSync('./keys/server.crt')
+	},
+	(req, res) => {
+		// res.writeHead(200, { 'Content-Type': 'text/html' });
+		// res.write('<h1>Hello</h1>');
+		// res.end();
 
-	if (req.url === '/') {
-		const htmlFile = readFileSync('./public/index.html', 'utf-8');
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.end(htmlFile);
-		return;
+		if (req.url === '/') {
+			const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.end(htmlFile);
+			return;
+		}
+
+		if (req.url?.endsWith('.js') ?? false) {
+			res.writeHead(200, { 'Content-Type': 'application/javascript' });
+		} else if (req.url?.endsWith('.css') ?? false) {
+			res.writeHead(200, { 'Content-Type': 'text/css' });
+		}
+
+		try {
+			const contentResponse = fs.readFileSync(`./public/${req.url}`, 'utf-8');
+			res.end(contentResponse);
+		} catch (error) {
+			res.writeHead(404, { 'Content-Type': 'text/html' });
+			res.end();
+		}
 	}
-
-	if (req.url?.endsWith('.js') ?? false) {
-		res.writeHead(200, { 'Content-Type': 'application/javascript' });
-	} else if (req.url?.endsWith('.css') ?? false) {
-		res.writeHead(200, { 'Content-Type': 'text/css' });
-	}
-
-	const contentResponse = readFileSync(`./public/${req.url}`, 'utf-8');
-	res.end(contentResponse);
-});
+);
 
 server.listen(3000, () => {
 	console.log('Server running on port 3000');
