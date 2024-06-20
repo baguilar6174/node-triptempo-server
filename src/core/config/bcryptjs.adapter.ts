58fc2a365a1383904c1ee1f34e5f76e12ead2405
Bryan Aguilar <baguilar@technisys.com>
2024-06-20 15:27:34 -0500

    🔨 refactor: Replace basic encrypt and token generations, now using jsonwetoken and bcryptjs dependencies

---
+++ src/core/config/bcryptjs.adapter.ts
@@ -0,0 +1,18 @@
+import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
+
+export const bcrypt = {
+	/**
+	 * Generates a hash for a password with a salt.
+	 * @param password - The password to hash.
+	 * @returns - The hashed password.
+	 */
+	hash: (password: string): string => hashSync(password, genSaltSync()),
+
+	/**
+	 * Compares a password with a given hash and salt.
+	 * @param password - The password to verify.
+	 * @param hash - The original hash to compare with.
+	 * @returns - True if the password matches, false otherwise.
+	 */
+	comparePassword: (password: string, hash: string): boolean => compareSync(password, hash)
+};
---
+++ src/core/config/jsonwebtoken.adapter.ts
@@ -0,0 +1,39 @@
+import { type JwtPayload, sign, verify } from 'jsonwebtoken';
+
+import { envs } from './envs.adapter';
+
+const JWT_SEED = envs.JWT_SEED;
+
+/**
+ * JWT adapter for basic authentication.
+ */
+export const jsonWebToken = {
+	/**
+	 * Creates a JWT token.
+	 * @param {Record<string, any>} payload - The payload of the token.
+	 * @param {string} duration - The token expiration time in seconds.
+	 * @returns {string} The generated JWT token.
+	 */
+	generateToken: async (payload: Record<string, unknown>, duration: string = '2h'): Promise<string | undefined> => {
+		return await new Promise((resolve) => {
+			sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
+				if (!err) resolve(token);
+				resolve(undefined);
+			});
+		});
+	},
+
+	/**
+	 * Verifies a JWT token.
+	 * @param {string} token - The JWT token to verify.
+	 * @returns {Record<string, any> | null} The decoded payload if the token is valid, otherwise null.
+	 */
+	validateToken: async <T>(token: string): Promise<T | JwtPayload | undefined> => {
+		return await new Promise((resolve) => {
+			verify(token, JWT_SEED, (err, decoded) => {
+				if (!err) resolve(decoded as T);
+				resolve(undefined);
+			});
+		});
+	}
+};
